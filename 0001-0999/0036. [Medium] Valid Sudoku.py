6c8936b3a5e8ff559eea93e6bd03d023f77e27f2
KeyXK <ttctde@gmail.com>
2022-05-25 10:26:08 +0700

    Solution 0036. [Medium] Valid Sudoku

---
+++ 0001-0999/0036. [Medium] Valid Sudoku.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/valid-sudoku
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        mats = defaultdict(set)
+        
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r]) or (board[r][c] in cols[c]) or (board[r][c] in mats[(r//3, c//3)]):
+                    return False
+                else:
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    mats[(r//3, c//3)].add(board[r][c])
+        return True
