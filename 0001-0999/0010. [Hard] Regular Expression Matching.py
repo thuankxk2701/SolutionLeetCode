780c1c0d6dc8adeaf986b544914cb2e8134c3b57
KeyXK <ttctde@gmail.com>
2022-05-25 09:27:10 +0700

    Solution 0010. [Hard] Regular Expression Matching

---
+++ 0001-0999/0010. [Hard] Regular Expression Matching.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/regular-expression-matching/
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        def dp(i, j):
+            if (i, j) not in seen:
+                if j == len(p):
+                    ans = i == len(s)
+                else: 
+                    first_match = i < len(s) and p[j] in s[i] + "."
+                    if j + 1 < len(p) and p[j + 1] == "*":
+                        ans = dp(i, j + 2) or first_match and dp(i + 1, j)
+                    else: 
+                        ans = first_match and dp(i + 1, j + 1)
+                
+                seen[(i, j )] = ans
+            
+            return seen[(i, j )]
+        
+        seen = {}
+        return dp(0, 0)
